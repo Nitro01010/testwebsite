c0770bd56255741d51a6f2d1afc93490546344f9
Nitro01010 <80020708+Nitro01010@users.noreply.github.com>
2022-02-03 09:57:46 -0800

    v1

--- scripts.js
+++ scripts.js
@@ -1,22 +1,75 @@
-let stopwatch = document.getElementById("start")
-l = document.createTextNode("asfddaf");
-stopwatch.innerHTML("adfsfsd")
-timer = 0;
-
-
-
-
-function innerStart(){
-	timer+=1;
-	i = timer.toString();
-	newTimer = document.createTextNode(i);
-	stopwatch.appendChild(newTimer);
-	document.body.appendChild(stopwatch);
+window.onload = function(){
+	stopwatch = document.getElementById("timer");
+	actualtimer = document.getElementById("actualtimer");
+	timer  = 0
+	powerstate = false;
+	x = 0
+	y = 0;
+	z = 0;
 }
+
+
+function output(){
+	stopwatch.innerHTML = `${y}:${z}`
+	actualtimer.innerHTML = timer;
+}
+function increment(){
+	timer++;
+}
+function floor(a){
+	Math.floor(a)
+}
+function innerStart(){
+	timer++;
+	if(Math.floor(timer / 100) >= 1){
+		y = Math.floor(timer/100);
+		if(y < 10){y = `0${y}`}
+	}
+	else{
+		y = "00"
+	}
+	z = timer -100*(Math.floor(timer/100));
+	if(z < 10){
+		z = `0${z}`;
+	}
+}
+
+
+
+
 function start(){
-	setInterval(innerStart,1000)
+	if(powerstate  == false){
+		interval = setInterval(innerStart,10)
+		powerstate = true
+	}
 }
 function stop(){
+	if(powerstate == true){
+		clearInterval(interval)
+		powerstate = false;
+	}
 }
 function reset(){
+	timer = -1;
+	innerStart();
+	powerstate = false;
+}
+
+
+
+
+
+
+
+function newStart(){
+	let decimaltimer = timer/100
+	let decimaltimerrounded = Math.floor(decimaltimer)
+	if(){
+		
+	}
+	else{
+		
+	}
+
+
 }
